3ad2e79b5e796c07d205e465056bc9c024fed287
ivanjermakov <vania6600@gmail.com>
2018-11-23 23:19:42 +0300

    controller fix

--- src/main/java/org/htpllang/controller/ExecuteController.java
+++ src/main/java/org/htpllang/controller/ExecuteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("compile")
+@RequestMapping("execute")
 public class ExecuteController {
 	
 	private final ExecuteService executeService;
